64f55858994c250b97ed5efb8dc785d11fd472b5
Dot-NotSam <sameer.24bai10364@vitbhopal.ac.in>
2026-06-24 23:51:00 +0530

    Create Solution.java

---
+++ Day-6/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String s : strs) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String curr = new String(c);
+
+            if(!map.containsKey(curr)) {
+                map.put(curr, new ArrayList<>());
+            }
+
+            map.get(curr).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
