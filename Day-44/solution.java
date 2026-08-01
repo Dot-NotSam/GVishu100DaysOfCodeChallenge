5264bba53284350cbc58f2e9212d2679dd5c0c36
Dot-NotSam <sameer.24bai10364@vitbhopal.ac.in>
2026-08-02 01:21:40 +0530

    Create solution.java

---
+++ Day-44/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+        int zero = 0;
+        int one = 0;
+        int ans = 0;
+        for(char c : s.toCharArray()) {
+            if(c=='0') zero++;
+            else if(c=='1') one++;
+
+            if(zero==one || Math.abs(zero-one)<2) ans++;
+        }
+
+        return ans;
+    }
+}
