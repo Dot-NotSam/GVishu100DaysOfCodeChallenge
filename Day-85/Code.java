12582a0dc1c08549ff0ae67e7485ee8681e486d6
Dot-NotSam <sameer.24bai10364@vitbhopal.ac.in>
2026-09-11 23:01:50 +0530

    Update Code.java

--- Day-85/Code.java
+++ Day-85/Code.java
@@ -1,2 +1,38 @@
-gf
-  
+class Solution {
+    int cnt = 0;
+
+    public int totalNumbers(int[] digits) {
+        int n = digits.length;
+        boolean[] visited = new boolean[n];
+        func(digits, n, new StringBuilder(), visited);
+        return cnt;
+    }
+
+    public void func(int[] arr, int n, StringBuilder num,
+                     boolean[] visited) {
+
+        if (num.length() == 3) {
+            if (Integer.parseInt(num.toString())%2 == 0) {
+                cnt++;
+            }
+            return;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+
+            if (set.contains(arr[i])) continue;
+            set.add(arr[i]);
+
+            if (num.length() == 0 && arr[i] == 0) continue;
+
+            visited[i] = true;
+            num.append(arr[i]);
+            func(arr, n, num, visited);
+            num.deleteCharAt(num.length()-1);
+            visited[i] = false;
+        }
+    }
+}
